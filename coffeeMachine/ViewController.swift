81fd341cb83b4fa08c762dd2a464b9a4edbe26f1
Regnauldy Tanri <regnauldyt@gmail.com>
2019-05-16 09:38:21 +0700

    Initial Commit

---
+++ coffeeMachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coffeeMachine
+//
+//  Created by Regnauldy Tanri on 16/05/19.
+//  Copyright © 2019 Regnauldy Tanri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
